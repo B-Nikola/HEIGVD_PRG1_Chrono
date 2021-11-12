69afdec889f8fc520e51c2018e1e899ff06fc33b
Antonio Pollino <antoniopollino99@gmail.com>
2021-11-12 13:32:34 +0100

    Main.cpp + classes
    Mise à jour du main
    Ajout de la bibliothèque chronomètre (fichier .h et .cpp)
    Ajout de la bibliothèque générateur aléatoire (fichier .h et .cpp)
    Mise à jour de l'en-tête avec explications

---
+++ generationAleatoire.cpp
@@ -0,0 +1,27 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier : <nom du fichier>.<xxx>
+Nom du labo : <nom du labo>
+Auteur(s) : <prénom> <nom>
+Date creation : <jj.mm.aaaa>
+Description : <à compléter>
+Remarque(s) : <à compléter>
+Compilateur : Mingw-w64 g++ 11.1.0
+-----------------------------------------------------------------------------------
+*/
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+#define ERASE_BUFFER cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+using namespace std;
+int main()
+{
+ <code>
+
+ cout << "Presser ENTER pour quitter";
+ ERASE_BUFFER;
+ return EXIT_SUCCESS;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
-#include <iostream>
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier : main
+Nom du labo : Labo 5 : reflex clavier
+Auteur(s) : Bouatit Nikola et Pollino Antonio
+Date creation : 10.11.2021
+Description :
+               Ce programme permet de tester la dextérité de l'utilisateur.
+               Le programme va afficher des lettres aléatoires, le nombre de
+               lettres varie en fonction de la saisie utilisateur.
+               l'utilisateur doit reproduire le plus rapidement possible la
+               séquence de lettre affichée par le programme. Dès que l'utilisateur
+               a terminé la saisie, le programme affiche le nombre d'entrées qui
+               ont été correctement réalisées, le temps total de l'exercice ainsi que
+               le temps moyen par lettre.
 
-int main() {
-   std::cout << "Hello, World!" << std::endl;
-   return 0;
+Remarque(s) : <à compléter>
+Compilateur : Mingw-w64 g++ 11.1.0
+-----------------------------------------------------------------------------------
+*/
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <limits>
+
+#define VIDER_BUFFER cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+using namespace std;
+int main()
+{
+   <code>
+
+      cout << "Presser ENTER pour quitter";
+   VIDER_BUFFER;
+   return EXIT_SUCCESS;
 }
